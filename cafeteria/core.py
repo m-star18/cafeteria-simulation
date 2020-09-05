a334ac422a6d83944270704f901e5e246fe81578
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-05 23:01:54 +0900

    Add Cafeteria

--- cafeteria/core.py
+++ cafeteria/core.py
@@ -10,3 +10,17 @@ SEED = 42
 
 SIT_SCORE = 100
 PENALTY_SCORE = [-20, -30, -10, -40, -50]
+
+
+class Cafeteria:
+
+    def __init__(self, data, time):
+        self.table = data[0]
+        self.number = data[1]
+        self.seats = data[2]
+
+        random.seed(SEED)
+        self.group_member = collections.deque([random.randint(1, MAX_MEMBER) for _ in range(SHOW_MEMBER)])
+        self.score = [0] * (time + 1)
+        self.flag = 0
+        self.index = 0
