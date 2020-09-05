42aca4b7c63bf75f34a62594103af28be8e474cd
m-star18 <ryuuchei@ezweb.ne.jp>
2020-09-05 23:01:05 +0900

    Add core.py

---
+++ cafeteria/core.py
@@ -0,0 +1,12 @@
+import random
+import collections
+import matplotlib.pyplot as plt
+
+
+SHOW_MEMBER = 10
+MAX_MEMBER = 7
+OUT_TIME = 20
+SEED = 42
+
+SIT_SCORE = 100
+PENALTY_SCORE = [-20, -30, -10, -40, -50]
