a32def263f8bd24024f0e322973ed21207ff9534
m-star18 <ryuuchei@ezweb.ne.jp>
2020-10-17 13:20:04 +0900

    Add sit_check

--- sample/puzzle.py
+++ sample/puzzle.py
@@ -10,3 +10,17 @@ def sit_check(number, count, flag=-1):
         max_index = 0
         table_count = 0
         index = 0
+        for j, seat in enumerate(table):
+            if seat == -1:
+                # indexをメモしておく
+                if table_count == 0:
+                    index = j
+                table_count += 1
+            else:
+                # 空いている数が最大のとき
+                if max_table_count < table_count:
+                    max_table_count = table_count
+                    max_index = index
+                # 初期化
+                table_count = 0
+                index = 0
